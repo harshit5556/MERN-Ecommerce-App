cb1d8aceb793cbd29b1640e200602ed66a5a4fc4
Harshit Tiwari <122154874+harshit5556@users.noreply.github.com>
2025-06-12 15:12:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,14 @@ const dotenv = require("dotenv").config();
 const Stripe = require('stripe')
 
 const app = express();
-app.use(cors());
+//app.use(cors());
+app.use(
+  cors({
+    origin: "https://mern-ecommerce-app-ig8a.vercel.app/",
+    credentials: true,
+  })
+);
+
 app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 8080;
