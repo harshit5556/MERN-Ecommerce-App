3a1382c1b5e4aa781769bd1d8554b3219d36f35c
Harshit Tiwari <122154874+harshit5556@users.noreply.github.com>
2025-07-15 22:57:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,11 @@ const dotenv = require("dotenv").config();
 const Stripe = require('stripe')
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://mern-ecommerce-app-fro.onrender.com",
+  credentials: true
+}));
+
 
 
 app.use(express.json({ limit: "10mb" }));
